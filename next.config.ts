5500071c5c09ddc030b36eee5de9f88aeb9944a5
Nikit Phadke <nikitph@gmail.com>
2025-06-05 19:43:31 +0530

    Bank onboarding flow

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/quotes/overview",
+        destination: "/en/saas/banksignup",
         permanent: true,
       },
     ];
